416836b618460667eae087196ca65af8d396c595
clever-girl31 <johnmseybold@gmail.com>
2023-04-18 15:43:16 -0700

    building click event, creating variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,13 @@
-apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=a5c9bbc780a6d5f3b81b7c2576a5552c'
-apiWeather = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=a5c9bbc780a6d5f3b81b7c2576a5552c&units=imperial'
+var latitude = 0
+var longitude = 0
+
+var apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a5c9bbc780a6d5f3b81b7c2576a5552c'
+
+var apiWeather = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=a5c9bbc780a6d5f3b81b7c2576a5552c&units=imperial'
 
 var searchButton = document.getElementById('search')
 searchButton.addEventListener('click', function() {
-  fetch()
+  var input = document.getElementById('city')
+  var city = input.value
+  console.log(city)
 })
