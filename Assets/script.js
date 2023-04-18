ad133064d594c22cd2b0bcf9e1409a4cb40766c8
clever-girl31 <johnmseybold@gmail.com>
2023-04-18 15:36:46 -0700

    setting up apis

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=a5c9bbc780a6d5f3b81b7c2576a5552c'
+apiWeather = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=a5c9bbc780a6d5f3b81b7c2576a5552c&units=imperial'
+
+var searchButton = document.getElementById('search')
+searchButton.addEventListener('click', function() {
+  fetch()
+})
