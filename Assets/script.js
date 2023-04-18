3c90e3cd2e75e5aec694bdeb969018af23aa2759
clever-girl31 <johnmseybold@gmail.com>
2023-04-18 15:59:25 -0700

    got a response from GeoApi. creating variables based on response

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,6 @@
 var latitude = 0
 var longitude = 0
 
-var apiGeo = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a5c9bbc780a6d5f3b81b7c2576a5552c'
-
 var apiWeather = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=a5c9bbc780a6d5f3b81b7c2576a5552c&units=imperial'
 
 var searchButton = document.getElementById('search')
@@ -10,4 +8,10 @@ searchButton.addEventListener('click', function() {
   var input = document.getElementById('city')
   var city = input.value
   console.log(city)
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a5c9bbc780a6d5f3b81b7c2576a5552c')
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => (console.error("error fetching")))
 })
