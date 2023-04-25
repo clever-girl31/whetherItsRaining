05aefe650847f67e0540504492f97321dc39b517
clever-girl31 <johnmseybold@gmail.com>
2023-04-25 17:17:14 -0500

    got icons to display

--- Assets/script.js
+++ Assets/script.js
@@ -18,12 +18,12 @@ searchButton.addEventListener('click', function() {
             date3: "(" + response.list[24].dt_txt.slice(0, 10) + ")",
             date4: "(" + response.list[32].dt_txt.slice(0, 10) + ")",
             date5: "(" + response.list[39].dt_txt.slice(0, 10) + ")",
-            icon0: response.list[0].weather[0].icon,
-            icon1: response.list[8].weather[0].icon,
-            icon2: response.list[16].weather[0].icon,
-            icon3: response.list[24].weather[0].icon,
-            icon4: response.list[32].weather[0].icon,
-            icon5: response.list[39].weather[0].icon,
+            icon0: "<img src='https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png' width='50' height='50'>",
+            icon1: "<img src='https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + ".png' width='50' height='50'>",
+            icon2: "<img src='https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png' width='50' height='50'>",
+            icon3: "<img src='https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + ".png' width='50' height='50'>",
+            icon4: "<img src='https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + ".png' width='50' height='50'>",
+            icon5: "<img src='https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png' width='50' height='50'>",
             temp0: "Temperature: " + response.list[0].main.temp + "°F",
             temp1: "Temperature: " + response.list[8].main.temp + "°F",
             temp2: "Temperature: " + response.list[16].main.temp + "°F",
@@ -50,7 +50,7 @@ searchButton.addEventListener('click', function() {
           // display icon
           // !make it an actual icon
           var resultIcon = document.getElementById("icon0")
-          resultIcon.textContent = cityData.icon0
+          resultIcon.innerHTML = cityData.icon0
           // current date
           var date0 = document.getElementById("searchedDate")
           date0.textContent = cityData.date0
@@ -68,7 +68,7 @@ searchButton.addEventListener('click', function() {
           var date1 = document.getElementById("date1")
           date1.textContent = cityData.date1
           var icon1 = document.getElementById("icon1")
-          icon1.textContent = cityData.icon1
+          icon1.innerHTML = cityData.icon1
           var temp1 = document.getElementById("temp1")
           temp1.textContent = cityData.temp1
           var wind1 = document.getElementById("wind1")
@@ -79,7 +79,7 @@ searchButton.addEventListener('click', function() {
           var date2 = document.getElementById("date2")
           date2.textContent = cityData.date2
           var icon2 = document.getElementById("icon2")
-          icon2.textContent = cityData.icon2
+          icon2.innerHTML = cityData.icon2
           var temp2 = document.getElementById("temp2")
           temp2.textContent = cityData.temp2
           var wind2 = document.getElementById("wind2")
@@ -90,7 +90,7 @@ searchButton.addEventListener('click', function() {
           var date3 = document.getElementById("date3")
           date3.textContent = cityData.date3
           var icon3 = document.getElementById("icon3")
-          icon3.textContent = cityData.icon3
+          icon3.innerHTML = cityData.icon3
           var temp3 = document.getElementById("temp3")
           temp3.textContent = cityData.temp3
           var wind3 = document.getElementById("wind3")
@@ -101,7 +101,7 @@ searchButton.addEventListener('click', function() {
           var date4 = document.getElementById("date4")
           date4.textContent = cityData.date4
           var icon4 = document.getElementById("icon4")
-          icon4.textContent = cityData.icon4
+          icon4.innerHTML = cityData.icon4
           var temp4 = document.getElementById("temp4")
           temp4.textContent = cityData.temp4
           var wind4 = document.getElementById("wind4")
@@ -112,7 +112,7 @@ searchButton.addEventListener('click', function() {
           var date5 = document.getElementById("date5")
           date5.textContent = cityData.date5
           var icon5 = document.getElementById("icon5")
-          icon5.textContent = cityData.icon5
+          icon5.innerHTML = cityData.icon5
           var temp5 = document.getElementById("temp5")
           temp5.textContent = cityData.temp5
           var wind5 = document.getElementById("wind5")
@@ -171,12 +171,12 @@ for (i = 0; i < searchCollectionList.length; i++) {
               date3: "(" + response.list[24].dt_txt.slice(0, 10) + ")",
               date4: "(" + response.list[32].dt_txt.slice(0, 10) + ")",
               date5: "(" + response.list[39].dt_txt.slice(0, 10) + ")",
-              icon0: response.list[0].weather[0].icon,
-              icon1: response.list[8].weather[0].icon,
-              icon2: response.list[16].weather[0].icon,
-              icon3: response.list[24].weather[0].icon,
-              icon4: response.list[32].weather[0].icon,
-              icon5: response.list[39].weather[0].icon,
+              icon0: "<img src='https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png' width='50' height='50'>",
+              icon1: "<img src='https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + ".png' width='50' height='50'>",
+              icon2: "<img src='https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + ".png' width='50' height='50'>",
+              icon3: "<img src='https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + ".png' width='50' height='50'>",
+              icon4: "<img src='https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + ".png' width='50' height='50'>",
+              icon5: "<img src='https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png' width='50' height='50'>",
               temp0: "Temperature: " + response.list[0].main.temp + "°F",
               temp1: "Temperature: " + response.list[8].main.temp + "°F",
               temp2: "Temperature: " + response.list[16].main.temp + "°F",
@@ -203,7 +203,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             // display icon
             // !make it an actual icon
             var resultIcon = document.getElementById("icon0")
-            resultIcon.textContent = cityData.icon0
+            resultIcon.innerHTML = cityData.icon0
             // current date
             var date0 = document.getElementById("searchedDate")
             date0.textContent = cityData.date0
@@ -221,7 +221,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             var date1 = document.getElementById("date1")
             date1.textContent = cityData.date1
             var icon1 = document.getElementById("icon1")
-            icon1.textContent = cityData.icon1
+            icon1.innerHTML = cityData.icon1
             var temp1 = document.getElementById("temp1")
             temp1.textContent = cityData.temp1
             var wind1 = document.getElementById("wind1")
@@ -232,7 +232,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             var date2 = document.getElementById("date2")
             date2.textContent = cityData.date2
             var icon2 = document.getElementById("icon2")
-            icon2.textContent = cityData.icon2
+            icon2.innerHTML = cityData.icon2
             var temp2 = document.getElementById("temp2")
             temp2.textContent = cityData.temp2
             var wind2 = document.getElementById("wind2")
@@ -243,7 +243,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             var date3 = document.getElementById("date3")
             date3.textContent = cityData.date3
             var icon3 = document.getElementById("icon3")
-            icon3.textContent = cityData.icon3
+            icon3.innerHTML = cityData.icon3
             var temp3 = document.getElementById("temp3")
             temp3.textContent = cityData.temp3
             var wind3 = document.getElementById("wind3")
@@ -254,7 +254,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             var date4 = document.getElementById("date4")
             date4.textContent = cityData.date4
             var icon4 = document.getElementById("icon4")
-            icon4.textContent = cityData.icon4
+            icon4.innerHTML = cityData.icon4
             var temp4 = document.getElementById("temp4")
             temp4.textContent = cityData.temp4
             var wind4 = document.getElementById("wind4")
@@ -265,7 +265,7 @@ for (i = 0; i < searchCollectionList.length; i++) {
             var date5 = document.getElementById("date5")
             date5.textContent = cityData.date5
             var icon5 = document.getElementById("icon5")
-            icon5.textContent = cityData.icon5
+            icon5.innerHTML = cityData.icon5
             var temp5 = document.getElementById("temp5")
             temp5.textContent = cityData.temp5
             var wind5 = document.getElementById("wind5")
